1f08d6a8bb5acb220f6d045d7a2c05712b16a1cc
Canon4G <gllawliet@163.com>
2018-12-13 11:14:23 +0800

    测试

--- src/main/java/com/example/demo/entity/Test.java
+++ src/main/java/com/example/demo/entity/Test.java
@@ -13,9 +13,6 @@ public class Test {
     private static Logger logger = LoggerFactory.getLogger(Test.class);
 
     public static void main(String[] args) {
-        logger.info("测试日志 info");
-        logger.error("测试日志 error");
-        logger.warn("测试日志 warn");
-
+        logger.info("测试日志 提交到github上");
     }
 }
